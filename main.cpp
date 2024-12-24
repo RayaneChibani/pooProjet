49ecaa7d2076a1cd5d8b2cab1a49f5f14c7880ed
RayaneChibani <112261918+RayaneChibani@users.noreply.github.com>
2024-12-24 12:27:03 +0100

    Update main.cpp avec l'algorithme de main droite

--- main.cpp
+++ main.cpp
@@ -1,17 +1,64 @@
 #include <iostream>
 #include "terrain.h"
 #include <stdlib.h>
+#include "robot.h"
+#include <thread> // Pour std::this_thread::sleep_for
+#include <chrono> // Pour std::chrono::milliseconds
+class EnregistreurRobot;
+
+void algorithmeMainDroite(robot& robot) {
+    int casesParcourues = 0; // Compteur des cases parcourues
+
+    while (true) {
+        // Afficher l'état actuel du terrain
+        robot.getTerrain().afficherTerrain();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Pause de 500 ms
+        system("cls");
+        // Tourner et avancer selon l'algorithme
+        robot.tournerDroite();
+        if (!robot.obstacleDevant()) {
+            robot.avancer();
+            casesParcourues++;
+        } else {
+            robot.tournerGauche();
+            if (!robot.obstacleDevant()) {
+                robot.avancer();
+                casesParcourues++;
+            } else {
+                robot.tournerGauche();
+            }
+        }
+
+        // Condition de sortie
+        if (robot.estSurSortie()) {
+            std::cout << "Le robot a trouvé la sortie !" << std::endl;
+            break;
+        }
+    }
+
+    // Afficher le nombre de cases parcourues
+    std::cout << "Nombre de cases parcourues : " << casesParcourues << std::endl;
+}
+
+
 
 int main()
 {
-    /*int x,y;
-    cout << "Donner la largeur : \n";
-    cin >>x;
-    cout << "Donner la longueur : \n";
-    cin >>y;
-    **/
-    std::string file="../lab1.txt";
+    std::string file="lab2.txt";
     terrain t{file};
-    t.afficher();
+    //t.afficherTerrain();
+
+                      // Crée un robot lié au terrain
+
+    // Ajouter un observateur pour suivre ses déplacements
+    //EnregistreurRobot obs;
+
+
+    // Appeler l'algorithme pour résoudre le labyrinthe
+
+    robot r{t};
+     //r.ajouterObservateur(&obs);
+    algorithmeMainDroite(r);
+
     return 0;
 }
