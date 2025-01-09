b50c491661f89ca51cb2aa0f3e25f82282d6d62f
RayaneChibani <112261918+RayaneChibani@users.noreply.github.com>
2025-01-09 15:31:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,54 +5,65 @@
 #include "observateur.h"
 #include <windows.h> // Pour Sleep (en millisecondes)
 #include <memory>
+#include <fstream>
+
+constexpr int tempsDattente = 500;
+
+void effacerFichier(const std::string& nomFichier) {
+    std::ofstream logFile(nomFichier, std::ios::trunc);  // Efface tout le contenu du fichier
+    logFile.close();
+}
 
 void afficheSimulation(robot& robot){
         robot.getTerrain().afficherTerrain();
-        Sleep(500);
+        Sleep(tempsDattente);
         system("cls");
 }
-int casesParcourrues(int &nombres){return nombres;}
 
-void algorithmeMainDroite(robot& robot, std::unique_ptr<observateurRobotDeplacements> obserRobotDeplacements) {
-    int casesParcourues{0};
+void sortieTrouvee(robot& robot){
+    robot.getTerrain().afficherTerrain();
+    std::cout << "Le robot a trouve la sortie !" << std::endl;
+
+    for (const auto& observateur : robot.getObservateurs()) {
+        auto* collecteur = dynamic_cast<observateurRobotDeplacements*>(observateur.get());
+            if (collecteur) {
+                   std::cout << "Nombre de cases parcourues : " << collecteur->nbDeplacements() << std::endl;
+            }
+    }
+}
+
+
+void algorithmeMainDroite(robot& robot) {
     while (true && !robot.estSurSortie()) {
         afficheSimulation(robot);
         robot.tournerDroite();
         if (!robot.obstacleDevant()) {
             robot.avancer();
-            casesParcourues++;
         } else {
             robot.tournerGauche();
             if (!robot.obstacleDevant()) {
                 robot.avancer();
-                casesParcourues++;
             } else {
                 robot.tournerGauche();
             }
         }
     }
-    robot.getTerrain().afficherTerrain();
-     std::cout << "Le robot a trouve la sortie !" << std::endl;
-     std::cout << "Nombre de cases parcourrues : " << casesParcourues << std::endl;
-     std::cout << "Nombre de cases parcourrues de la part de l'observateur : " << obserRobotDeplacements->nbDeplacements() << std::endl;
+    sortieTrouvee(robot);
+
 }
 
 
 
 int main()
 {
+    effacerFichier("deplacements.log");
+
     std::string file="lab2.txt";
     terrain terrain{file};
     robot test_robot{terrain};
-
-
-
-    std::unique_ptr<observateurRobotDeplacements> obserRobotDeplacements = std::make_unique<observateurRobotDeplacements>();
-
-
     test_robot.ajouterObservateur(std::make_unique<observateurRobotDirection>());
-    test_robot.ajouterObservateur(std::move(obserRobotDeplacements));
+    test_robot.ajouterObservateur(std::make_unique<observateurRobotDeplacements>());
 
-    algorithmeMainDroite(test_robot, std::move(obserRobotDeplacements));
+    algorithmeMainDroite(test_robot);
     return 0;
 }
